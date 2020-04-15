29e8909fbd2662a67c46de1b68106276f8db3ca7
huyaocode <2385249676@qq.com>
2020-04-15 17:45:43 +0800

    feat: 富文本quill引入

--- client/src/Pages/edit/index.tsx
+++ client/src/Pages/edit/index.tsx
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { NextPage } from "next";
 import { WithTranslation } from "next-i18next";
-import { NamespacesRequiredProps } from "@Interfaces";
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
 import "./styles.scss";
 import { PageLayout } from "@/Components";
+import { NamespacesRequiredProps } from "@Interfaces";
 
+const QuillNoSSRWrapper = dynamic(import("react-quill"), {
+    ssr: false,
+    loading: () => <p>Loading ...</p>,
+});
 
-const Edit: NextPage<
-    WithTranslation,
-    NamespacesRequiredProps
-> = ({ t, i18n }) => {
+const modules = {
+    toolbar: [
+        [{ header: "1" }, { header: "2" }],
+        [{ size: [] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" },
+        ],
+        ["link", "image"],
+        ["clean"],
+    ],
+    clipboard: {
+        // toggle to add extra line breaks when pasting HTML:
+        matchVisual: false,
+    },
+};
+
+const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+];
+
+const Edit: NextPage<WithTranslation, NamespacesRequiredProps> = ({
+    t,
+    i18n,
+}) => {
+    const [value, setValue] = useState("");
     return (
         <PageLayout t={t} i18n={i18n}>
-            Edit
+            <QuillNoSSRWrapper
+                modules={modules}
+                formats={formats}
+                theme="snow"
+                onChange={setValue}
+            />
         </PageLayout>
     );
 };
